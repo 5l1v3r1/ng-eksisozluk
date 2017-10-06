f16e1ebd7855d34ab220a418073c76fe17bf556f
Deniz YesilIrmak <dnzyslrmk@gmail.com>
2017-10-06 23:01:00 +0300

    popular and today routing

--- src/app/services/today.service.ts
+++ src/app/services/today.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers   } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TodayService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
+
+  getAllPopular(){
+    return this._http.get("https://eksi-sozluk.herokuapp.com/today")
+    .map(res => res.json());
+  }
 
 }
--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TodayService } from '../services/today.service';
 @Component({
+  moduleId: module.id,
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.css']
 })
+
 export class TodayComponent implements OnInit {
+  Topics: any[];
+  popularFound: boolean = false;
+  searching: boolean = false;
 
-  constructor() { }
+  
 
-  ngOnInit() {
+  handleSuccess(data){
+    this.popularFound = true;
+    this.Topics = data.Topics;
+    console.log(data.hits);
   }
+
+  handleError(error){
+    console.log(error);
+  }
+
+  constructor(private _todService:TodayService) { }
+
+  getPopular(){
+    return this._todService.getAllPopular().subscribe(
+      data => this.handleSuccess(data),
+      error => this.handleError(error)
+    )
+  }
+  
+  ngOnInit() { 
+    this.getPopular();
+  }
+
+  
+
+
+
+  
 
 }
